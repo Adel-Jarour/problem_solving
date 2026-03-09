27cc13c22e836d004145d836d38e77efd4f3dd0c
adel <adel@gmail.com>
2026-03-09 15:30:21 +0200

    solve problem (5. Longest Palindromic Substring)

---
+++ 5.Longest-Palindromic-Substring/solution.js
@@ -0,0 +1,29 @@
+var longestPalindrome = function (s) {
+  if (s.length === 0) return "";
+
+  let start = 0,
+    end = 0;
+
+  function expandAroundCenter(s, left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandAroundCenter(s, i, i);
+    let len2 = expandAroundCenter(s, i, i + 1);
+    let len = len1 > len2 ? len1 : len2;
+
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+
+  return s.substring(start, end + 1);
+};
+
+console.log(longestPalindrome("cbbd"));
--- README.md
+++ README.md
@@ -14,3 +14,4 @@ I will use one of these languages ​​in the solution:
 2. `add-two-numbers` | Dart
 3. `longest-substring-without-repeating-characters` | Dart
 4. `Median-of-Two-Sorted-Arrays` | JavaScript
+5. `Longest-Palindromic-Substring` | JavaScript
